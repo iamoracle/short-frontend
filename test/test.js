5e89afd8e65e967a1b632f9d8580dfbe3db220fa
iamoracle <38940247+iamoracle@users.noreply.github.com>
2021-01-20 00:17:41 +0100

    Added a sample test

--- test/test.js
+++ test/test.js
@@ -1,1 +1,9 @@
-/* add test later */
+import { mount } from '@vue/test-utils'
+import Home from '@/components/home.vue'
+
+describe('Home', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Home)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
